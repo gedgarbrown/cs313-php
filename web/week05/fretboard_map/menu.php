546df4a7f4023d3f9ae8196602e667ef8b7dfb60
GeorgeCavaco <georgecavaquinho@hotmail.com>
2019-10-19 23:52:47 -0600

    adding database again

--- web/week05/fretboard_map/menu.php
+++ web/week05/fretboard_map/menu.php
@@ -55,11 +55,35 @@
 					<select name="InstrumentID">
 						<?php
 							
-							//TODO get available options from Database
-							
-							echo '<option value="0">Guitar</option>';
-							echo '<option value="1">Cavaquinho</option>';
+							//setup database
+					try
+					{
+						$dbUrl = getenv('DATABASE_URL');
+
+						$dbOpts = parse_url($dbUrl);
+
+						$dbHost = $dbOpts["host"];
+						$dbPort = $dbOpts["port"];
+						$dbUser = $dbOpts["user"];
+						$dbPassword = $dbOpts["pass"];
+						$dbName = ltrim($dbOpts["path"],'/');
+
+						$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	
+						$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);				
+					}
+						catch (PDOException $ex)
+					{
+						echo 'Error!: ' . $ex->getMessage();
+						die();
+					}
+	
 					
+					foreach ($db->query('SELECT id, name FROM instruments') as $row)
+					{
+						echo '<option value="'.$row["id"].'>'.$row["name"].'</option>';				
+							
+					}		
 					
 						?>
 					</select><br>
@@ -83,10 +107,35 @@
 					<select name="InstrumentID">
 						<?php
 							
-							//TODO get available options from Database
+							//setup database
+					try
+					{
+						$dbUrl = getenv('DATABASE_URL');
+
+						$dbOpts = parse_url($dbUrl);
+
+						$dbHost = $dbOpts["host"];
+						$dbPort = $dbOpts["port"];
+						$dbUser = $dbOpts["user"];
+						$dbPassword = $dbOpts["pass"];
+						$dbName = ltrim($dbOpts["path"],'/');
+
+						$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	
+						$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);				
+					}
+						catch (PDOException $ex)
+					{
+						echo 'Error!: ' . $ex->getMessage();
+						die();
+					}
+	
+					
+					foreach ($db->query('SELECT id, name FROM instruments') as $row)
+					{
+						echo '<option value="'.$row["id"].'>'.$row["name"].'</option>';				
 							
-							echo '<option value="0">Guitar</option>';
-							echo '<option value="1">Cavaquinho</option>';
+					}		
 					
 					
 						?>
@@ -140,11 +189,35 @@
 					<select name="InstrumentID">
 						<?php
 							
-							//TODO get available options from Database
-							
-							echo '<option value="0">Guitar</option>';
-							echo '<option value="1">Cavaquinho</option>';
+							//setup database
+					try
+					{
+						$dbUrl = getenv('DATABASE_URL');
+
+						$dbOpts = parse_url($dbUrl);
+
+						$dbHost = $dbOpts["host"];
+						$dbPort = $dbOpts["port"];
+						$dbUser = $dbOpts["user"];
+						$dbPassword = $dbOpts["pass"];
+						$dbName = ltrim($dbOpts["path"],'/');
+
+						$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	
+						$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);				
+					}
+						catch (PDOException $ex)
+					{
+						echo 'Error!: ' . $ex->getMessage();
+						die();
+					}
+	
 					
+					foreach ($db->query('SELECT id, name FROM instruments') as $row)
+					{
+						echo '<option value="'.$row["id"].'>'.$row["name"].'</option>';				
+							
+					}		
 					
 						?>
 					</select><br>
